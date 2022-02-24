c4d475ae7b9c51ad58fdeb50e017935b3f0c09b7
berkegenckaya <berkegenckaya@gmail.com>
2022-02-24 11:15:00 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,25 +1,27 @@
-
-const contactForm = document.getElementById('contact-form');
+const contactForm = document.getElementById("contact-form");
 let name1 = document.getElementById("nameSurname");
 let email = document.getElementById("email");
 let message = document.getElementById("message");
 
 contactForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    
-    const formData ={ 
-        name: name1.value,
-        email: email.value,
-        message: message.value,
-    }
-    console.log(formData);
-   
-    fetch("https://inthecasualmailserver.herokuapp.com", {
-        method: "POST",
-        body: JSON.stringify(formData),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
-    
-})
+  e.preventDefault();
+
+  const formData = {
+    name: name1.value,
+    email: email.value,
+    message: message.value,
+  };
+  console.log(formData);
+
+  fetch(
+    "https://inthecasualmailserver.herokuapp.com",
+    {
+      method: "POST",
+      body: JSON.stringify(formData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    },
+    { mode: "no-cors" }
+  );
+});
